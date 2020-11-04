aa0fa9cb5a47fd57d6b43459e5c25e8aeaef8b91
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-11-04 10:51:25 +0200

    hold

---
+++ src/views/Challenge/ChallengeContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Challenge from "./Challenge";
+
+export default class ChallengesContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      challenge: [{
+        name: "tree",
+        summary: "implement a tree data structure with all traversal method",
+        _id: "1234",
+      }],
+      solution: null,
+      userId: "123",
+
+    };
+  }
+
+  render() {
+    const { challenge, solution, userId } = this.state;
+    return <Challenge challenge={challenge} solution={solution} userId={userId} />;
+  }
+}
