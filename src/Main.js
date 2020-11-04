03936a9dd21089b03ce35b2aad28c2a53b5639e0
laith <mr.laith_2011@hotmail.com>
2020-11-04 14:52:00 +0200

    comment

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./views/Home/Home";
-import Login from "./views/Login/LoginContainer";
+// import Login from "./views/Login/LoginContainer";
 import Signup from "./views/signup/SignupContainer";
+import Comment from "./views/Comment/CommentContainer";
 
 import PageNotFound from "./views/PageNotFound/PageNotFound";
 
@@ -10,10 +11,10 @@ function Main() {
   return (
     <main>
       <Switch>
-        <Route exact path="/login" component={Login} />
+        {/* <Route exact path="/login" component={Login} /> */}
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/comment" component={Comment} />
         <Route exact path="/" component={Home} />
-
         <Route path="*" component={PageNotFound} />
       </Switch>
     </main>
