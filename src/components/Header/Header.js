4f6502b2634bd3cf9cde85e829ebdcb2bbb4a566
laith <mr.laith_2011@hotmail.com>
2020-11-04 12:41:34 +0200

    edit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,26 +9,6 @@ import './header.scss';
 function Header() {
   return (
     <main id="headerPage">
-
-      {/* <Navbar className="containerHeader" collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Coodak-front</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Link to="/">
-            <Nav.Link href="#features">Home</Nav.Link>
-          </Link>
-
-          <Link to="/signup">
-            <Nav.Link href="#Signup">REGISTER/SIGNIN</Nav.Link>
-          </Link>
-           <Link to="/signin">
-            <Nav.Link href="#signin">signin</Nav.Link>
-          </Link> 
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>  */}
-
       <Navbar>
         <ul class="menuItems">
         <Navbar.Brand id="anker" href="#home">Coodak</Navbar.Brand>
