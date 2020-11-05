2b4f299ec72f6f7fe4d67f8d4e2b22b700ca8388
amuammer <amuammer@students.iugaza.edu.ps>
2020-11-05 14:53:20 +0200

    adding code editor

--- src/views/CodeEditor/CodeEditor.js
+++ src/views/CodeEditor/CodeEditor.js
@@ -2,21 +2,22 @@
 
 import React from "react";
 
-function CodeEdotor(props) {
-  const { userId, project } = props;
-  return (
-    <>
-      <textarea id="html" placeholder="HTML" name="code_html" />
+import "./CodeEditor.scss";
 
-      <textarea id="css" placeholder="CSS" name="code_css" />
-      <textarea id="js" placeholder="JavaScript" name="code_js" />
+function CodeEdotor(props) {
+  const { userId, project, onKeyUp } = props;
+  return (
+    <main id="CodeEditor">
+      <textarea name="code_html" id="html" onKeyUp={onKeyUp} placeholder="HTML" />
+      <textarea name="code_css" id="css" onKeyUp={onKeyUp} placeholder="CSS" />
+      <textarea name="code_js" id="js" onKeyUp={onKeyUp} placeholder="JavaScript" />
       <input type="hidden" name="owner_id" value={userId} />
       <label htmlFor="name">
         <input type="text" name="name" id="projectName" value={project.name} required />
       </label>
 
       <iframe id="code" title="preview" />
-    </>
+    </main>
   );
 }
 
