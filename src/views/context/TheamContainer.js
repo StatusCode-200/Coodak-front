b0ab8a717c162bd659ccb89e6fe3d925db48ae71
laith <mr.laith_2011@hotmail.com>
2020-11-06 17:31:51 +0200

    newBranch

---
+++ src/views/context/TheamContainer.js
@@ -0,0 +1,30 @@
+/*eslint-disable*/
+
+import React from 'react';
+
+export const ThemeContext = React.createContext();
+
+class ThemeProvider extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'light',
+            
+            toggleMode: this.toggleMode
+        }
+    }
+
+    toggleMode = () => this.setState({mode : this.state.mode == 'dark' ? 'light' : 'dark'});
+
+    render() {
+        return (
+            <ThemeContext.Provider value={this.state}>
+                {this.props.children}
+            </ThemeContext.Provider>
+        )
+      
+    }
+
+}
+
+export default ThemeProvider;
