214ece0100a36d1df464f7c6be3911a26e9a4e34
laith <mr.laith_2011@hotmail.com>
2020-11-06 16:59:10 +0200

    update

--- src/views/context/TheamContainer.js
+++ src/views/context/TheamContainer.js
@@ -8,7 +8,7 @@ class ThemeProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            mode: 'dark',
+            mode: 'light',
             toggleMode: this.toggleMode
         }
     }
