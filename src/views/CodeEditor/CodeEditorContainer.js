1f80ceb13933a369b58763fc2694e2a22aa9fc24
amuammer <amuammer@students.iugaza.edu.ps>
2020-11-10 15:40:16 +0200

    connect code editor

--- src/views/CodeEditor/CodeEditorContainer.js
+++ src/views/CodeEditor/CodeEditorContainer.js
@@ -3,7 +3,7 @@
 import React, { Component } from "react";
 import CodeEditor from "./CodeEditor";
 import { connect } from "react-redux";
-import { getProjectAction, postProjectAction, putProjectAction } from "./../../store/actions/project.js"
+import { getProjectAction, postProjectAction, putProjectAction, flushProjectAction } from "./../../store/actions/project.js"
 import Loading from "../../components/Loading/Loading";
 
 class CodeEdotorContainer extends Component {
@@ -11,53 +11,38 @@ class CodeEdotorContainer extends Component {
     super(props);
     this.state = {
       project: {
-        _id: null,
-        owner_id: null,
-        name: null,
-        code_html: null,
-        code_css: null,
-        code_js: null,
+        name: "",
+        code_html: "",
+        code_css: "",
+        code_js: "",
       },
-
     };
   }
 
   componentWillMount() {
     if(this.props.match.params.projectId){
-      this.props.getProjectAction({projectId : this.props.match.params.projectId,userId : this.props.userId, token: this.props.token});
-    }else{
-      this.setState({ project: {...this.state.project, owner_id: this.props.userId} });
+      this.props.getProject({projectId : this.props.match.params.projectId,userId : this.props.userId, token: this.props.token});
     }
-
 }
 
 componentWillReceiveProps(nextProps){
-  // set state project at the firstTime (after loaded success)
-  // if (JSON.stringify(nextProps.project) !== JSON.stringify(this.props.project)){
-  //   this.setState({ project: {...nextProps.project} });
-  // }
-  console.log("componentWillReceiveProps");
-  if(this.props.match.params.projectId){
-    console.log(this.props.match.params.projectId);
-  this.setState({ project: {...nextProps.project} });
-  }else{
-    console.log("should reset it");
+  if (!this.props.project._id && nextProps.project._id){
+    this.props.history.push(`/projects/${nextProps.project._id}`);
+  } else if (!nextProps.match.params.projectId) {  // reset data on switch between opened project and code editor
+     this.props.flushProject();
   }
+  this.setState({ project: {...nextProps.project} });
 }
-
- componentWillUnmount(){
-   console.log("adnan");
-   console.log("adnan");
- }
 
   handleSubmit = (e) => {
     const { project } = this.state;
     const { userId, token } = this.props;
     e.preventDefault();
-    if(this.props.project._id){
-      this.props.putProjectAction({ project, projectId: this.props.match.params.projectId, userId, token });
+    const projectId = this.props.match.params.projectId;
+    if(projectId){
+      this.props.putProject({ project, projectId, userId, token });
     }else{
-      this.props.postProjectAction({ project, userId, token });
+      this.props.postProject({ project, userId, token });
     }
   }
   handleChange = e => {
@@ -91,7 +76,6 @@ componentWillReceiveProps(nextProps){
   render() {
     const { isLoading } = this.props;
     const { project } = this.state;
-    console.log("params", this.props.match.params.projectId);
     return (
       <>
         { isLoading ?
@@ -106,13 +90,15 @@ componentWillReceiveProps(nextProps){
 }
 
 const mapDispatchToProps = {
-  getProjectAction: getProjectAction,
-  postProjectAction: postProjectAction,
-  putProjectAction: putProjectAction,
+  getProject: getProjectAction,
+  postProject: postProjectAction,
+  putProject: putProjectAction,
+  flushProject: flushProjectAction,
 }
 
 const mapStateToProps = store => ({
   project: store.project.project,
+  insertedId: store.project.insertedId,
   isLoading: store.project.isLoading,
   userId: store.auth.user._id,//you might get it from auth from user object
   token: store.auth.token,
